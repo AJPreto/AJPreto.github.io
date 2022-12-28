75b4848b0c5a88ddfeb448c47b392cffd68a67a7
AJPreto <martinsgomes.jose@gmail.com>
2022-12-28 20:22:10 +0000

    Reinstated
    reinstated the code lines with the connection to html

--- assets/js/maintable_test.js
+++ assets/js/maintable_test.js
@@ -12,14 +12,14 @@ function require(script) {
     });
 }
 
+import tablecsv from "./tablecsv.js";
+const tableRoot = document.querySelector("#csv_table");
+const tableCsv = new tablecsv(tableRoot);
+
 
 var csv = require("./jquery.csv.js");
 
 /**
- * 
- * import tablecsv from "./tablecsv.js";
- * const tableRoot = document.querySelector("#csv_table");
- * const tableCsv = new tablecsv(tableRoot);
  * var data = $.csv.toObjects("./data/events_organised.csv");
  */
 
