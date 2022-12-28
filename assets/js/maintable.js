4edca2204042d33273d4fb71b8880bd7e03da0bb
AJPreto <martinsgomes.jose@gmail.com>
2022-12-28 21:18:49 +0000

    Simplified accronyms
    Removed extensive descriptions

--- assets/js/maintable.js
+++ assets/js/maintable.js
@@ -4,12 +4,12 @@ const tableRoot = document.querySelector("#csv_table");
 const tableCsv = new tablecsv(tableRoot);
 
 const organized_events_data = [
-	["December 2021", "VIII Encontro de Jovens Investigadores de Biologia Computacional Estrutural - Coimbra, December 20th", "Co-organiser"],
+	["December 2021", "VIII EJIBCE - Coimbra, December 20th", "Co-organiser"],
 	["May 2020", "Webinars cycle during the COVID-19 pandemic, 'Para além da pandemia', organised by the National Biochemists' Association (ANBIOQ)", "Co-organiser"],
-	["December 2019", "VII Encontro de Jovens Investigadores de Biologia Computacional Estrutural - Lisbon, December 20th", "Co-organiser"],
-	["December 2018", "VI Encontro de Jovens Investigadores de Biologia Computacional Estrutural - Oporto, December 21st", "Co-organiser"],
+	["December 2019", "VII EJIBCE - Lisbon, December 20th", "Co-organiser"],
+	["December 2018", "VI EJIBCE - Oporto, December 21st", "Co-organiser"],
 	["April 2018", "R workshop by JEST - Coimbra, April 23th-26th", "Co-organiser"],
-	["December 2017", "V Encontro de Jovens Investigadores de Biologia Computacional Estrutural - Coimbra, December 22nd", "Co-organiser"],
+	["December 2017", "V EJIBCE - Coimbra, December 22nd", "Co-organiser"],
 	["November 2017", "MYD - Mind Your Data - first edition, by JEST. Data science centered conference with the participation of Feedzai, Critical Software, Fraunhofer among others - Coimbra, November 25th", "Lead organiser"],
 	["May 2017", "IX Biochemistry Student’s Meeting", "Lead organiser"],
 	["March 2017", "Bioempreende o teu Futuro - Coimbra, by NEBIOQ/AAC, March 10th-12th", "Lead organiser"],
