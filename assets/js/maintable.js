9d58befc918d0c88b05fb86c6da1b82df7b2738a
AJPreto <martinsgomes.jose@gmail.com>
2022-12-28 21:22:34 +0000

    Link update
    put links in bold and added some more

--- assets/js/maintable.js
+++ assets/js/maintable.js
@@ -4,12 +4,12 @@ const tableRoot = document.querySelector("#csv_table");
 const tableCsv = new tablecsv(tableRoot);
 
 const organized_events_data = [
-	["December 2021", "VIII <a href='http://ejibce.github.io/'>EJIBCE</a> - Coimbra, December 20th", "Co-organiser"],
-	["May 2020", "Webinars cycle during the COVID-19 pandemic, 'Para além da pandemia', organised by the National Biochemists' Association (ANBIOQ)", "Co-organiser"],
-	["December 2019", "VII EJIBCE - Lisbon, December 20th", "Co-organiser"],
-	["December 2018", "VI EJIBCE - Oporto, December 21st", "Co-organiser"],
+	["December 2021", "VIII <a href='http://ejibce.github.io/' target='_blank'><b>EJIBCE</b></a> - Coimbra, December 20th", "Co-organiser"],
+	["May 2020", "Webinars cycle during the COVID-19 pandemic, 'Para além da pandemia', organised by ANBIOQ", "Co-organiser"],
+	["December 2019", "VII <a href='http://ejibce.github.io/' target='_blank'><b>EJIBCE</b></a> - Lisbon, December 20th", "Co-organiser"],
+	["December 2018", "VI <a href='http://ejibce.github.io/' target='_blank'><b>EJIBCE</b></a> - Oporto, December 21st", "Co-organiser"],
 	["April 2018", "R workshop by JEST - Coimbra, April 23th-26th", "Co-organiser"],
-	["December 2017", "V EJIBCE - Coimbra, December 22nd", "Co-organiser"],
+	["December 2017", "V <a href='http://ejibce.github.io/' target='_blank'><b>EJIBCE</b></a> - Coimbra, December 22nd", "Co-organiser"],
 	["November 2017", "MYD - Mind Your Data - first edition, by JEST. Data science centered conference with the participation of Feedzai, Critical Software, Fraunhofer among others - Coimbra, November 25th", "Lead organiser"],
 	["May 2017", "IX Biochemistry Student’s Meeting", "Lead organiser"],
 	["March 2017", "Bioempreende o teu Futuro - Coimbra, by NEBIOQ/AAC, March 10th-12th", "Lead organiser"],
