8fe4d6a79c75c27bbba660eed1c1b9435aa58067
AJPreto <martinsgomes.jose@gmail.com>
2022-12-28 20:34:47 +0000

    Gave up csv import
    Wrote events directly on script

--- assets/js/maintable.js
+++ assets/js/maintable.js
@@ -3,21 +3,11 @@ import tablecsv from "./tablecsv.js";
 const tableRoot = document.querySelector("#csv_table");
 const tableCsv = new tablecsv(tableRoot);
 
-/**
- * var data = $.csv.toObjects("./data/events_organised.csv");
- */
-
-$.ajax({
-    url: "./data/events_organised.csv",
-    async: false,
-    success: function (csvd) {
-        data = $.csv.toArrays(csvd);
-    },
-    dataType: "text",
-    complete: function () {
-        // call a function on complete 
-    }
-});
+const organized_events_data = [
+	["2020/05/01 - 2020/05/23", "Webinars cycle during the COVID-19 pandemic, 'Para além da pandemia', organised by the National Biochemists' Associaton (ANBIOQ)", "Co-organiser"],
+	["2018", "Encontro de Jovens Investigadores de Biologia Computacional Estrutural", "Co-organiser"],
+	["2017", "MYD - Mind Your Data - first edition. Data science centered conference with the participation of Feedzai, Critical Software, Fraunhofer among others", "Lead organiser"],
+	["2015", "IX Biochemistry Student’s Meeting", "Lead organiser"]]
 
 tableCsv.update(data,
 ["Date", "Description", "Role"]
