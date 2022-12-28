d88e1aef0df80cde111bc55bc34f4ced06797de1
AJPreto <martinsgomes.jose@gmail.com>
2022-12-28 15:27:20 +0000

    Corrected maintable code
    removed ":" replaced with ";"

--- assets/js/maintable.js
+++ assets/js/maintable.js
@@ -3,7 +3,7 @@ import tablecsv from "./tablecsv.js";
 const tableRoot = document.querySelector("#csv_table");
 const tableCsv = new tablecsv(tableRoot);
 
-var data = $.csv.toObjects("./data/events_organised.csv"):
+var data = $.csv.toObjects("./data/events_organised.csv");
 
 tableCsv.update(data,
 ["Date", "Description", "Role"]
