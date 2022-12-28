01dcd5efc619b4dbad01da9a906fe310464c2396
AJPreto <martinsgomes.jose@gmail.com>
2022-12-28 20:17:10 +0000

    tried adding explicit require function
    added a require function instead of just calling it

--- assets/js/maintable_test.js
+++ assets/js/maintable_test.js
@@ -1,5 +1,19 @@
+function require(script) {
+    $.ajax({
+        url: script,
+        dataType: "script",
+        async: false,           // <-- This is the key
+        success: function () {
+            // all good...
+        },
+        error: function () {
+            throw new Error("Could not load script " + script);
+        }
+    });
+}
 
-import toArrays from "./jquery.csv.js"
+
+var csv = require("./jquery.csv.js");
 
 /**
  * 
