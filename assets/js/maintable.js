715ef058ee2e6d8e746def1633b95eb42413fbd5
AJPreto <martinsgomes.jose@gmail.com>
2022-12-28 21:14:58 +0000

    Organized events update
    Added organized events

--- assets/js/maintable.js
+++ assets/js/maintable.js
@@ -4,10 +4,19 @@ const tableRoot = document.querySelector("#csv_table");
 const tableCsv = new tablecsv(tableRoot);
 
 const organized_events_data = [
-	["2020/05/01 - 2020/05/23", "Webinars cycle during the COVID-19 pandemic, 'Para além da pandemia', organised by the National Biochemists' Associaton (ANBIOQ)", "Co-organiser"],
-	["2018", "Encontro de Jovens Investigadores de Biologia Computacional Estrutural", "Co-organiser"],
-	["2017", "MYD - Mind Your Data - first edition. Data science centered conference with the participation of Feedzai, Critical Software, Fraunhofer among others", "Lead organiser"],
-	["2015", "IX Biochemistry Student’s Meeting", "Lead organiser"]]
+	["December 2021", "VIII Encontro de Jovens Investigadores de Biologia Computacional Estrutural - Coimbra, December 20th", "Co-organiser"],
+	["May 2020", "Webinars cycle during the COVID-19 pandemic, 'Para além da pandemia', organised by the National Biochemists' Association (ANBIOQ)", "Co-organiser"],
+	["December 2019", "VII Encontro de Jovens Investigadores de Biologia Computacional Estrutural - Lisbon, December 20th", "Co-organiser"],
+	["December 2018", "VI Encontro de Jovens Investigadores de Biologia Computacional Estrutural - Oporto, December 21st", "Co-organiser"],
+	["April 2018", "R workshop by JEST - Coimbra, April 23th-26th", "Co-organiser"],
+	["December 2017", "V Encontro de Jovens Investigadores de Biologia Computacional Estrutural - Coimbra, December 22nd", "Co-organiser"],
+	["November 2017", "MYD - Mind Your Data - first edition, by JEST. Data science centered conference with the participation of Feedzai, Critical Software, Fraunhofer among others - Coimbra, November 25th", "Lead organiser"],
+	["May 2017", "IX Biochemistry Student’s Meeting", "Lead organiser"],
+	["March 2017", "Bioempreende o teu Futuro - Coimbra, March 10th-12th", "Lead organiser"],
+	["May 2016", "Bioempreende o teu Futuro - Coimbra, May 20th-22nd", "Co-organiser"]]
+
+const participated_events_data = [
+	["March 2015", "Bioempreende o teu Futuro - Coimbra, March 6th-8th", "Contestant"]]
 
 tableCsv.update(organized_events_data,
 )
