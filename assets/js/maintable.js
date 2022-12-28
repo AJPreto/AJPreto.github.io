3511c7d077482f42b18cb01fb9d2d81890e81d89
AJPreto <martinsgomes.jose@gmail.com>
2022-12-28 02:46:54 +0000

    Normalize code
    update code to be more similar to example

--- assets/js/maintable.js
+++ assets/js/maintable.js
@@ -1,6 +1,6 @@
 import tablecsv from "./tablecsv.js"; 
 
 const tableRoot = document.querySelector("#csv_table")
-const tableCsv = new tablecsv(tableRoot)
+const tablecsv = new tablecsv(tableRoot)
 
-tableCsv.setHeader(["Date", "Description", "Role"])
+tablecsv.setHeader(["Date", "Description", "Role"])
